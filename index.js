18976bcfa0da1e9b6d9d6f54a2c24b87faf02f06
NOFUCIDKDJDK <152839808+NOFUCIDKDJDK@users.noreply.github.com>
2024-01-27 21:54:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `lIVE ON TWTICH`;
+      const newDetails = `/yxn`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
